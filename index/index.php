89227f83b79775a93e687fbe89152f920c7e1520
LeoPhilip33 <leo.philip@mmibordeaux.com>
2020-04-05 15:44:41 +0200

    send

---
+++ index/index.php
@@ -0,0 +1,15 @@
+<?php
+    include '../navbar/navbar.php';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" rel="stylesheet" href="../navbar/stylenavbar.css">
+    <title>La Suite d'Elisabeth</title>
+</head>
+<body>
+    
+</body>
+</html>
