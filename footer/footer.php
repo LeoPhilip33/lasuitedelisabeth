a2fb3b64cfe8e4c53a0b6547ecb2880835f60ad6
LeoPhilip33 <leo.philip@mmibordeaux.com>
2020-04-06 16:40:11 +0200

    Footer terminé

--- footer/footer.php
+++ footer/footer.php
@@ -9,40 +9,44 @@
 <body>
     <footer>
         <div class="footerdelimitation">
-            <div class="footerflex">
-                <iframe class="footerplan" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d176.67141413393057!2d1.216934249660819!3d44.88788640981808!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aca9d1327f9df9%3A0xa841886249f73645!2s18%20Rue%20Tourny%2C%2024200%20Sarlat-la-Can%C3%A9da!5e0!3m2!1sfr!2sfr!4v1574189733341!5m2!1sfr!2sfr" width="320" height="200" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+            <div class="footerdelimitationintermediaire">
+                <div class="footerflex">
+                    <iframe class="footerplan" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d176.67141413393057!2d1.216934249660819!3d44.88788640981808!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aca9d1327f9df9%3A0xa841886249f73645!2s18%20Rue%20Tourny%2C%2024200%20Sarlat-la-Can%C3%A9da!5e0!3m2!1sfr!2sfr!4v1574189733341!5m2!1sfr!2sfr" width="320" height="200" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                </div>
+                <div class="footerflex espacementmobilefooter">
+                    <div class="footerflexflex">
+                        <div> <img src="../images/footer/location.png" alt="Localisation de l'appartement à louer à Sarlat"> </div>
+                        <div class="Roboto footerespacement footercentrerverticalement"> 18 rue Tourny 24200 Sarlat-la-Canéda </div>
+                    </div>
+                    <div class="footerflexflex">
+                        <div> <img src="../images/footer/email.png" alt="Localisation de l'appartement à louer à Sarlat"> </div>
+                        <div class="Roboto footerespacement footercentrerverticalement"> lasuitedelisabeth@gmail.com </div>
+                    </div>
+                    <div class="footerflexflex">
+                        <div> <img src="../images/footer/phone.png" alt="Localisation de l'appartement à louer à Sarlat"> </div>
+                        <div class="Roboto footerespacement footercentrerverticalement"> 06 89 57 25 55 ou 06 15 30 93 90 </div>
+                    </div>
+                </div>
             </div>
-            <div class="footerflex">
-                <div class="footerflexflex">
-                    <div> <img src="../images/footer/location.png" alt="Localisation de l'appartement à louer à Sarlat"> </div>
-                    <div> 18 rue Tourny 24200 Sarlat-la-Canéda </div>
+            <div class="footerdelimitationintermediaire">
+                <div class="footerflex">
+                    <div class="footerflexflex espacementmobilefooter">
+                        <div class="Roboto footerflexflex"> <a href="#" class="footerliens"> Mentions légales </a> </div>
+                        <div class="Roboto footerespacement footerflexflex"> <a href="#" class="footerliens"> CGU </a> </div>
+                    </div>
+                    <div class="Roboto footerflexflex"> <a href="#" class="footerliens"> Politique de confidentialité </a> </div>
+                    <div class="Roboto footerflexflex"> @2020 La Suite d'Elisabeth </div>
                 </div>
-                <div class="footerflexflex">
-                    <div> <img src="../images/footer/email.png" alt="Localisation de l'appartement à louer à Sarlat"> </div>
-                    <div> lasuitedelisabeth@gmail.com </div>
-                </div>
-                <div class="footerflexflex">
-                    <div> <img src="../images/footer/phone.png" alt="Localisation de l'appartement à louer à Sarlat"> </div>
-                    <div> 06 89 57 25 55 ou 06 15 30 93 90 </div>
-                </div>
-            </div>
-            <div class="footerflex">
-                <div class="footerflexflex">
-                    <div>Mentions légales</div>
-                    <div> CGU </div>
-                </div>
-                <div>Politique de confidentialité</div>
-                <div>@2020 La Suite d'Elisabeth</div>
-            </div>
-            <div class="footerflex" >
-                <div> Inscrivez-vous à notre newsletter </div>
-                <div class="footerflexflex">
-                    <div><input class="footernewsletter" type="email" placeholder="Votre adresse e-mail"></div>
-                    <div> <input class="boutonfooter" type="button" value="M'inscrire"> </div>
-                </div>
-                <div class="footerflexflex">
-                    <div><input type="checkbox" ></div>
-                    <div> J'accepte que mon adresse email soit enregistrée afin de recevoir les newsletters et emailings de La Suite d'Elisabeth </div>
+                <div class="footerflex espacementmobilefooter" >
+                    <div class="Roboto footerfont"> Inscrivez-vous à notre newsletter </div>
+                    <div class="footerflexflex">
+                        <div><input class="footernewsletter" type="email" placeholder="Votre adresse e-mail"></div>
+                        <div> <input class="boutonfooter Roboto" type="button" value="M'inscrire"> </div>
+                    </div>
+                    <div class="footerflexflex">
+                        <div><input type="checkbox"></div>
+                        <div class="Roboto footerespacement checkboxlabel"> J'accepte que mon adresse email soit enregistrée afin de recevoir les newsletters et emailings de La Suite d'Elisabeth </div>
+                    </div>
                 </div>
             </div>
         </div>
