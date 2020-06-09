ceaf520e694d05524acdd5be1dd46f81f2d90950
LeoPhilip33 <leo.philip@mmibordeaux.com>
2020-06-09 17:28:41 +0200

    aled

--- js/nav-js.js
+++
@@ -1,9 +0,0 @@
-var burger = document.querySelector('.icon');
-var menu = document.querySelector('.menu-mobile');
-var menuM = document.querySelector('.menuM');
-
-burger.onclick = function(){
-	burger.classList.toggle('active');
-	menu.classList.toggle('active');
-	menuM.classList.toggle('active');
-}
